6bcaa00d93c4cb68b2dbabfcca99ef9510640131
AnteVol <ante.volarevic@fer.hr>
2025-01-29 17:20:40 +0100

    Added cors

--- qr-scanner/server.js
+++ qr-scanner/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
+app.use(cors());
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
