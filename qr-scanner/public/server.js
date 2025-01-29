e791833484787c1ab90e0be8aa7ea1a26678ec3f
AnteVol <ante.volarevic@fer.hr>
2025-01-29 15:17:23 +0100

    Updated package.json

--- qr-scanner/public/server.js
+++ qr-scanner/public/server.js
@@ -1,19 +1,15 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(cors());
-// Serve static files from project root
-app.use(express.static(path.join(__dirname)));
 
-// Route for root path
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Start server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
